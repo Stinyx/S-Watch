3ae5215b0d57827940f1e5dd429ab203d2d2dfbe
Stinyx <stinyx1@gmail.com>
2026-02-14 17:58:58 +0100

    added 7-segment display clock style

--- src/Swatch_custom_firmware.cpp
+++ src/Swatch_custom_firmware.cpp
@@ -5,6 +5,8 @@
 #include "GxEPD2_display_selection_new_style.h"
 #include "images.h"
 
+#include <math.h>
+
 // Button2 
 #include "Button2.h"
 #define BUTTON_PIN 33
@@ -12,6 +14,9 @@
 // NTP (Network Time Protocol)
 #include <WiFi.h>
 #include "time.h"
+
+#define DISPLAYWIDTH 200
+#define DISPLAYHEIGHT 200
 
 // Input variables
 Button2 button;
@@ -25,12 +30,15 @@ const long gmtOffset_sec = 3600;
 const int daylightOffset_sec = 3600;
 const char* ntpServer = "pool.ntp.org";
 
-#define DISPLAYWIDTH 200
-#define DISPLAYHEIGHT 200
+// DEBUG SETTINGS
+const int startupProcedure = 1;
+const int currentClockStyle = 1; //0 is first
 
 unsigned long lastUpdate = 0;
 int lastConnectionUpdate = 0;
 int timeoutTimer = 0;
+
+
 
 enum WatchState {
   NTPSYNCING,
@@ -39,6 +47,37 @@ enum WatchState {
   SETTINGS,
   WEEZO,
   WEEZODRAWN,
+};
+
+unsigned char* digitArray[10] = {
+  digit0,
+  digit1,
+  digit2,
+  digit3,
+  digit4,
+  digit5,
+  digit6,
+  digit7,
+  digit8,
+  digit9
+};
+
+unsigned char* digitArray2[10] = {
+  digit0_1,
+  digit1_1,
+  digit2_1,
+  digit3_1,
+  digit4_1,
+  digit5_1,
+  digit6_1,
+  digit7_1,
+  digit8_1,
+  digit9_1
+};
+
+unsigned char** clockStyles[2] = {
+  digitArray, 
+  digitArray2
 };
 
 enum WatchState currentState;
@@ -75,36 +114,71 @@ String return_local_time_from_tm(struct tm t){
   return String(buf);
 }
 
+void draw_digit(int x, int y, int digit, int style){
+    display.drawXBitmap(x, y, clockStyles[style][digit], digit_width, digit_height, GxEPD_BLACK);
+}
+
 void draw_time(){
   struct tm timeinfo;  
+  char timeToString[4];
 
-  if( !getLocalTime(&timeinfo) ) {   
-    Serial.println("Unable to get time data");
+  int winX = 16;
+  int winY = 50;
+  int winW = 168; 
+  int winH = digit_height; 
+  int digitSpacing = 5;
+
+  if( !getLocalTime(&timeinfo) ) { 
+    sprintf(timeToString, "0000"); 
+    display.setPartialWindow(winX, winY, winW, winH);
+    display.firstPage();
+    do {
+        int offset = 0;
+        for(int i = 0; i < 4; i++){
+            int digit = timeToString[i] - '0';
+            draw_digit(winX + offset, winY, digit, currentClockStyle);
+
+            if(i == 1){
+                offset += digit_width + 3;
+                display.drawXBitmap(winX + offset, winY + (digit_height - dots_height)/2, dots, dots_width, dots_height, GxEPD_BLACK);
+                offset += dots_width + 3;
+            }else{
+                offset += digit_width + digitSpacing;
+            }
+        }
+    } while (display.nextPage());
     return;
   }
 
+  /*
   int year   = timeinfo.tm_year + 1900;
   int month  = timeinfo.tm_mon + 1;
   int day    = timeinfo.tm_mday;
+  */
 
   int hour   = timeinfo.tm_hour;
   int minute = timeinfo.tm_min;
   int second = timeinfo.tm_sec;
 
-  int winX = 20;
-  int winY = 50;
-  int winW = 150;
-  int winH = 80;
+  sprintf(timeToString, "%02d%02d", hour, minute); 
 
   display.setPartialWindow(winX, winY, winW, winH);
   display.firstPage();
   do {
-    display.fillRect(winX, winY, winW, winH, GxEPD_WHITE);
-    display.setCursor(winX + 20, winY + 60); 
-    //display.printf("%02d:%02d:%02d", hour, minute, second);
-    display.printf("%02d:%02d", hour, minute);
-  } while (display.nextPage());
+      int offset = 0;
+      for(int i = 0; i < 4; i++){
+          int digit = timeToString[i] - '0';
+          draw_digit(winX + offset, winY, digit, currentClockStyle);
 
+          if(i == 1){
+              offset += digit_width + 3;
+              display.drawXBitmap(winX + offset, winY + (digit_height - dots_height)/2, dots, dots_width, dots_height, GxEPD_BLACK);
+              offset += dots_width + 3;
+          }else{
+              offset += digit_width + digitSpacing;
+          }
+      }
+  } while (display.nextPage());
 }
 
 void draw_text_static(String text, int x, int y){
@@ -207,102 +281,106 @@ void setup() {
   // Refresh display once
   refresh_display();
 
-  // Draw WIFI Status message
-  display.setFullWindow();
-  display.firstPage();
-  do {
-    display.setCursor(0, 10);
-    display.println("CONNECTING");
-    display.println("TO WIFI...");
-  } while (display.nextPage());
+  if(startupProcedure){
+    //NTP and WIFI connection setup
+    unsigned long wifiStart = millis();
+    unsigned long lastDraw  = 0;
+    const unsigned long WIFI_TIMEOUT = 10000; 
+    int resultTextPosition = 60;
+    int resultTexPositionCursor = resultTextPosition + 25;
 
-  //NTP and WIFI connection setup
-  unsigned long wifiStart = millis();
-  unsigned long lastDraw  = 0;
-  const unsigned long WIFI_TIMEOUT = 10000; 
-  int resultTextPosition = 60;
-  int resultTexPositionCursor = resultTextPosition + 25;
+    // Draw WIFI Status message
+    display.setFullWindow();
+    display.firstPage();
+    do {
+      display.setCursor(0, 10);
+      display.println("CONNECTING");
+      display.println("TO WIFI...\n");
+      display.printf("%d/%d", millis() - wifiStart, WIFI_TIMEOUT);
+    } while (display.nextPage());
 
-  WiFi.begin(ssid, password);
+    WiFi.begin(ssid, password);
 
-  while (WiFi.status() != WL_CONNECTED) {
+    while (WiFi.status() != WL_CONNECTED) {
 
-    // Try to connect to wifi for 10s, else timeout and display error message
-    if (millis() - wifiStart > WIFI_TIMEOUT) {
+      // Try to connect to wifi for 10s, else timeout and display error message
+      if (millis() - wifiStart > WIFI_TIMEOUT) {
 
+        display.setPartialWindow(0, resultTextPosition, 200, 100);
+        display.firstPage();
+        do {
+          display.setCursor(0, resultTexPositionCursor);
+          display.println("FAILED TO");
+          display.println("CONNECT TO");
+          display.println("WIFI!");
+        } while (display.nextPage());
+
+        delay(5000);
+        break; 
+      }
+
+      delay(200); 
+    }
+
+    struct tm timeinfo;
+    int retries = 0;  
+
+    // Display that wifi was connected
+    display.setPartialWindow(0, resultTextPosition, 200, 100);
+    display.firstPage();
+    do {
+      display.setCursor(0, resultTexPositionCursor);
+      display.println("WIFI");
+      display.println("CONNECTED");
+      display.println("SUCCESSFULLY!");
+    } while (display.nextPage());
+    delay(1000);
+
+    configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+
+    // Display NTP Waiting status message
+    display.setFullWindow();
+    display.firstPage();
+    do {
+      display.fillScreen(GxEPD_WHITE);
+      display.setCursor(0, 10);
+      display.println("WAITING");
+      display.println("FOR NTP");
+      display.println("RESPONSE...\n");
+      display.printf("%d/%d", 1, 1);
+    } while (display.nextPage());
+
+    // Try to get NTP Data
+    while(!getLocalTime(&timeinfo) && retries < 10){
+        delay(1000);
+        retries++;
+    }
+
+    // Display if NTP was synced succesfully or not
+    if (retries == 10){
       display.setPartialWindow(0, resultTextPosition, 200, 100);
       display.firstPage();
       do {
         display.setCursor(0, resultTexPositionCursor);
         display.println("FAILED TO");
-        display.println("CONNECT TO");
-        display.println("WIFI!");
+        display.println("GET NTP DATA!");
       } while (display.nextPage());
-
-      delay(5000);
-      break; 
+    } else {
+      display.setPartialWindow(0, resultTextPosition, 200, 100);
+      display.firstPage();
+      do {
+        display.setCursor(0, resultTexPositionCursor);
+        display.println("NTP TIME");
+        display.println("ACQUIRED");
+        display.println("SUCCESFULLY!");
+      } while (display.nextPage());
     }
+    delay(2000);
 
-    delay(200); 
+    // Disconnect wifi for power saving
+    WiFi.disconnect(true);
+    WiFi.mode(WIFI_OFF);
   }
-
-  struct tm timeinfo;
-  int retries = 0;  
-
-  // Display that wifi was connected
-  display.setPartialWindow(0, resultTextPosition, 200, 100);
-  display.firstPage();
-  do {
-    display.setCursor(0, resultTexPositionCursor);
-    display.println("WIFI");
-    display.println("CONNECTED");
-    display.println("SUCCESSFULLY!");
-  } while (display.nextPage());
-  delay(1000);
-
-  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
-
-  // Display NTP Waiting status message
-  display.setFullWindow();
-  display.firstPage();
-  do {
-    display.fillScreen(GxEPD_WHITE);
-    display.setCursor(0, 10);
-    display.println("WAITING");
-    display.println("FOR NTP");
-    display.println("RESPONSE...");
-  } while (display.nextPage());
-
-  // Try to get NTP Data
-  while(!getLocalTime(&timeinfo) && retries < 10){
-      delay(1000);
-      retries++;
-  }
-
-  // Display if NTP was synced succesfully or not
-  if (retries == 10){
-    display.setPartialWindow(0, resultTextPosition, 200, 100);
-    display.firstPage();
-    do {
-      display.setCursor(0, resultTexPositionCursor);
-      display.println("FAILED TO");
-      display.println("GET NTP DATA!");
-    } while (display.nextPage());
-  } else {
-    display.setPartialWindow(0, resultTextPosition, 200, 100);
-    display.firstPage();
-    do {
-      display.setCursor(0, resultTexPositionCursor);
-      display.println("NTP TIME");
-      display.println("ACQUIRED");
-      display.println("SUCCESFULLY!");
-    } while (display.nextPage());
-  }
-  delay(2000);
-
-  // Disconnect wifi for power saving
-  WiFi.disconnect(true);
-  WiFi.mode(WIFI_OFF);
 }
 
 void onStateEnter(WatchState state) {
